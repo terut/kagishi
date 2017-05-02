bf98d8cb7ae0ac5e036ce2fd4059ff051578b7b6
terut <terut.dev+github@gmail.com>
2017-05-02 00:48:52 -0700

    Add expired? method

--- lib/kagishi/verifier.rb
+++ lib/kagishi/verifier.rb
@@ -12,11 +12,19 @@ module Kagishi
       parse(token)
     end
 
+    def expired_token?
+      verify_token
+    end
+
     # options:
     #  - `verify_expiration: false`: ignore expiration
     #  - `leeway: 15 * 60 (seconds)`: have leeway for expiration
     def payload
       parse(token, options: { leeway: exp_leeway })
+    end
+
+    def expired_payload?
+      payload
     end
 
     private
